3feee95ee4f3d227c800900719ad8e94b0b77f39
MatDV <matdv@matdvs-macbook-pro.home>
2024-08-21 17:22:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [switch1, setSwitch1] = useState(false);
+  const [switch2, setSwitch2] = useState(false);
+  const [switch3, setSwitch3] = useState(false);
+
+  return (
+    <>
+      <header>Ready To Go</header>
+      <main>
+        <section>
+          <div style={{ color: switch1 ? "purple" : "black" }}>
+            <button onClick={setSwitch1(switch1)}>On</button>
+          </div>
+          <div>
+            <button>Off</button>
+          </div>
+        </section>
+        <section>
+          <div>
+            <button>On</button>
+          </div>
+          <div>
+            <button>Off</button>
+          </div>
+        </section>
+        <section>
+          <div>
+            <button>On</button>
+          </div>
+          <div>
+            <button>Off</button>
+          </div>
+        </section>
+        <div>No WAY</div>
+        <div>GO</div>
+      </main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
